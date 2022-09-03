ba6e8d1fe6d15a3c36211f9213cc212c43e3ac24
Kanel04 <thelegendanelka@gmail.com>
2022-09-03 12:51:51 +0300

    template and child process

---
+++ src/utils/childprocess.js
@@ -0,0 +1,14 @@
+
+const exectute = (commande)  =>{
+    return new Promise((resolve , reject) =>{
+        exec(commande,(error, stdout,stderr) =>{
+            if (error){
+                return reject(error);
+            }
+            if (stderr){
+                return reject(stderr);
+            }
+            return resolve(stdout);
+        });
+    })
+};
